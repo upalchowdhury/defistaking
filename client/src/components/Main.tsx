b1bdaf88e17a6183d7d51d0e2d0004cd026c5cd4
upalchowdhury <upal.chowdhury@outlook.com>
2021-10-08 10:02:29 -0400

    ui

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,18 +1,42 @@
 import {useEthers} from "@usedapp/core";
 import helperConfig from "../helper-config.json"
 import networkMapping from "../chain-info/deployments/map.json"
-
+import dai from "../dai.png"
+import { YourWallet } from "./yourWallet/YourWallet";
 
 export const Main = () => {
 
     const {chainId, error} =useEthers()
     const networkName = chainId ? helperConfig[chainId] : "dev"
     
-    const dappTokenAddress = 
+    const dappTokenAddress = chainId ? networkMapping[stringChainId]["DappToken"][0] : constants.AddressZero
+    const wethTokenAddress = chainId ? brownieConfig["networks"][networkName]["weth_token"] : constants.AddressZero // brownie config
+    const fauTokenAddress = chainId ? brownieConfig["networks"][networkName]["fau_token"] : constants.AddressZero
 
+    const supportedTokens:Array<Token> = [
+        {
+            image: dapp,
+            address: dappTokenAddress,
+            name: "DAPP"
+        },
+        {
+            image:eth,
+            address: wethTokenAddress,
+            name: "WETH"
+        },
+        {
+            image:fau,
+            address: fauTokenAddress,
+            name: "DAI"
+        }
+
+    ]
     
     return (
-        <div></div>
+        <div>
+
+            <YourWallet supportedTokens/>
+        </div>
     )
 
 }
---
+++ client/src/components/yourWallet/YourWallet.tsx
@@ -0,0 +1,10 @@
+import {Token} from "../Main"
+
+interface YourWalletProps {
+    supportedToken
+}
+
+
+export const YourWallet = ({supportedToken}: YourWalletProps) => {
+
+}
